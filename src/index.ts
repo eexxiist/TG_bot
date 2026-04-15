14b7f329b4d3ddd395b0e8f099a47872506942a2
eexxiist <mego_dilya@mail.ru>
2026-04-15 22:15:27 +0300

    add ci yml

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-import {bot} from './bot'
+import { bot } from "./bot";
+import { GlobalContext, YCContext } from "./config/context";
 
-bot.start({onStart: () => {
-  console.log('Start Bot')
-}})
+export const handler = async function (event: any, context: YCContext) {
+    GlobalContext.context = context;
+
+    try {
+        await bot.init();
+        await bot.handleUpdate(JSON.parse(event.body));
+    } catch (e) {
+        console.error("Failed to handle update", (e as Error).message);
+    }
+
+    return { statusCode: 200, body: "" };
+};
+
+if (process.env.NODE_ENV === "development") {
+    bot.start({
+        onStart: () => {
+            console.log("Start Bot");
+        },
+    });
+}
